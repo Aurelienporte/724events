a78f5f12308cf747b3316f21d68cf276d4001bbc
Aurelienporte <140103588+Aurelienporte@users.noreply.github.com>
2024-02-19 11:53:48 +0100

    DaaContext BUG FIXED : last variable was missing. Variable added + parser function getLastEvent

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -9,19 +9,37 @@ import {
 
 const DataContext = createContext({});
 
+function compareDates (eventA, eventB){
+  if(eventA.date > eventB.date){
+    return 1;
+  }
+  if(eventA.date < eventB.date){
+    return -1;
+  }
+  return 0;
+}
+
 export const api = {
   loadData: async () => {
     const json = await fetch("/events.json");
     return json.json();
   },
+  loadLast: async () => {
+    const json = await fetch("/events.json");
+    const dataSet = await json.json();
+    const lastEvent = dataSet.events.sort(compareDates).pop();
+    return lastEvent;
+  }
 };
 
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null)
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
+      setLast(await api.loadLast());
     } catch (err) {
       setError(err);
     }
@@ -37,6 +55,7 @@ export const DataProvider = ({ children }) => {
       value={{
         data,
         error,
+        last
       }}
     >
       {children}
